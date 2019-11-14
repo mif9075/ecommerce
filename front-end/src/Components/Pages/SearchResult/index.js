0601cd1f668f62e2f13143586126dd5e0929c2bd
DavidSuy <DavidSuyDev@gmail.com>
2019-11-14 14:31:04 -0500

    updated searchpage

--- front-end/src/Components/Pages/SearchResult/index.js
+++ front-end/src/Components/Pages/SearchResult/index.js
@@ -4,9 +4,23 @@ import { connect } from "react-redux";
 class SearchResult extends Component {
   render() {
     console.log(this.props.searchResults);
+
+    let display = <div></div>;
+    if (this.props.searchResults === 404) {
+      display = (
+        <div>
+          <h2>Not Found</h2>
+          <p>Please check Username or Album and make sure it spell correctly</p>
+        </div>
+      );
+    }
     return (
       <div>
-        {this.props.searchResults.username || this.props.searchResults.name}
+        <h1>Search Result:</h1>
+        {display}
+        <h3>
+          {this.props.searchResults.username || this.props.searchResults.name}
+        </h3>
       </div>
     );
   }
