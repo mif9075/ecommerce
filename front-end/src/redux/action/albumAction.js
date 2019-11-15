2c1c7a3c0a13440731af67a3f4a9989829919395
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-11-15 12:04:54 -0500

    Added Album Actions

--- front-end/src/redux/action/albumAction.js
+++ front-end/src/redux/action/albumAction.js
@@ -1,6 +1,22 @@
-import { CREATE_ALBUM, ERROR_CREATE_ALBUM, GET_ALL_USER_ALBUMS} from '../actionTypes/index';
+import { CREATE_ALBUM, ERROR_CREATE_ALBUM, GET_ALL_USER_ALBUMS, GET_ALL_ALBUMS, GET_ALBUMS_BY_ID, DELETE_USER_ALBUMS_BY_ID } from '../actionTypes/index';
 
 import Axios from '../../lib/Axios';
+
+export const deleteUserAlbumByID = (id) => async dispatch => {
+    try {
+        let deletedByID = await Axios.delete(`/album/delete-by-id/${id}`);
+
+        dispatch({
+            type: DELETE_USER_ALBUMS_BY_ID,
+            payload: deletedByID,
+            id
+        })
+    } catch (error) {
+        console.log(error)
+        dispatch(errorCreateAlbum(error))
+        return Promise.reject(error);
+    }
+}
 
 export const getAllUserAlbums = (id) => async dispatch => {
     try {
@@ -42,6 +58,24 @@ export const createAlbum = (albumInfo) => async dispatch => {
     }
 }
 
+export const getAllAlbums = () => async dispatch => {
+
+    try {
+
+        let success = await Axios.get(`/album/get-all-albums`)
+
+        dispatch({
+            type: GET_ALL_ALBUMS,
+            payload: success.data
+        })
+        return Promise.resolve(success.data)
+    } catch (error) {
+        console.log(error)
+        dispatch(errorCreateAlbum(error))
+        return Promise.reject(error);
+    }
+}
+
 const successCreateAlbum = (createdAlbum) => dispatch => {
     dispatch({
         type: CREATE_ALBUM,
@@ -54,5 +88,23 @@ const errorCreateAlbum = (message) => dispatch => {
         type: ERROR_CREATE_ALBUM,
         payload: message
     })
+}
+
+export const getAlbumByID = (id) => async dispatch => {
+
+    try {
+
+        let foundAlbum = await Axios.get(`/album/get-album-by-id/${id}`)
+
+        dispatch({
+            type: GET_ALBUMS_BY_ID,
+            payload: foundAlbum.data
+        });
+
+        return Promise.resolve(foundAlbum.data)
+    } catch (error) {
+        dispatch(errorCreateAlbum(error));
+        return Promise.reject(error);
+    }
 }
 
